6a54dbe070a48e5bdead81b948df60b6ac168b82
Zephyr <tanygupt360@gmail.com>
2026-08-11 09:03:48 +0530

    Initial Commit

---
+++ src/model.py
@@ -0,0 +1,9 @@
+class VCRModel:
+    def __init__(self):
+        pass
+
+    def predict_answer(self, image, question, answers):
+        pass
+
+    def predict_rationale(self, image, question, answer, rationales):
+        pass
